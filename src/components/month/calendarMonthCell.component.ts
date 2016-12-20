fab6e0bd746026656f861f64767c0bae032c5cbc
Charles Vanderhoff <charles@vandersoft.co>
2016-12-19 21:27:07 -0800

    feat: cp

--- src/components/month/calendarMonthCell.component.ts
+++ src/components/month/calendarMonthCell.component.ts
@@ -5,23 +5,20 @@ import { MonthViewDay } from 'calendar-utils';
   selector: 'mwl-calendar-month-cell',
   template: `
     <div class="cal-cell-top">
-      <span class="cal-day-badge" *ngIf="day.badgeTotal > 0">{{ day.badgeTotal }}</span>
       <span class="cal-day-number">{{ day.date | calendarDate:'monthViewDayNumber':locale }}</span>
     </div>
-    <div class="cal-events">
-      <div
-        class="cal-event"
-        *ngFor="let event of day.events"
-        [style.backgroundColor]="event.color.primary"
-        [ngClass]="event?.cssClass"
-        (mouseenter)="highlightDay.emit({event: event})"
-        (mouseleave)="unhighlightDay.emit({event: event})"
-        [mwlCalendarTooltip]="event | calendarEventTitle:'monthTooltip'"
-        [tooltipPlacement]="tooltipPlacement"
-        mwlDraggable
-        [dropData]="{event: event}"
-        [dragAxis]="{x: event.draggable, y: event.draggable}">
-      </div>
+    <div
+      class="cal-event"
+      *ngFor="let event of day.events"
+      [style.backgroundColor]="event.color.primary"
+      [ngClass]="event?.cssClass"
+      (mouseenter)="highlightDay.emit({event: event})"
+      (mouseleave)="unhighlightDay.emit({event: event})"
+      [mwlCalendarTooltip]="event | calendarEventTitle:'monthTooltip'"
+      [tooltipPlacement]="tooltipPlacement"
+      mwlDraggable
+      [dropData]="{event: event}"
+      [dragAxis]="{x: event.draggable, y: event.draggable}">
     </div>
   `,
   host: {
